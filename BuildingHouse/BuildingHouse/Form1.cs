9c912421526e570d22159708ebf25415586b1122
nester <nesterd85@gmail.com>
2017-01-09 10:17:29 +0700

    на форме добавлены поля помещений создан метод инициализации данных полей метод помещен в конструктор формы

--- BuildingHouse/BuildingHouse/Form1.cs
+++ BuildingHouse/BuildingHouse/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BuisnesLogic.HouseItems;
+using BuisnesLogic.HouseItems.Base;
 
 namespace BuildingHouse
 {
     public partial class Form1 : Form
     {
+        RoomWithDoor livingRoom;
+        RoomWithDoor kitchen;
+        Room diningRoom;
+        OutsideWithDoor backYard;
+        OutsideWithDoor frontYard;
+        Outside garden;
         public Form1()
         {
             InitializeComponent();
+            CreateObjects();
+        }
+
+        private void CreateObjects()
+        {
+            livingRoom = new RoomWithDoor("Гостинная", "Старинный ковер", "Дубовая дверь с латунной ручкой");
+            kitchen = new RoomWithDoor("Кухня", "Плита из нержавеющей стали", "Сетчатая дверь");
+            diningRoom = new Room("Столовая", "Хрустальная люстра");
+            backYard = new OutsideWithDoor("Задний двор", true, "Сетчатая дверь");
+            frontYard = new OutsideWithDoor("Лужайка", false, "Дубовая дверь с латунной ручкой");
+            garden = new Outside("Сад", false);
+
+            livingRoom.exits = new Location[] { frontYard, diningRoom };
+            kitchen.exits = new Location[] { diningRoom, backYard };
+            diningRoom.exits = new Location[] { livingRoom, kitchen };
+            backYard.exits = new Location[] { kitchen, garden };
+            frontYard.exits = new Location[] { livingRoom, garden };
+            garden.exits = new Location[] { frontYard, backYard };
         }
     }
 }
