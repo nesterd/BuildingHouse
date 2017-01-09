d1eac41b38afcf289a4d69889ed2933c0d604795
nester <nesterd85@gmail.com>
2017-01-09 09:56:50 +0700

    описаны производные классы

---
+++ BuildingHouse/BuisnesLogic/HouseItems/OutsideWithDoor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuisnesLogic.HouseItems.Base;
+
+namespace BuisnesLogic.HouseItems
+{
+    public class OutsideWithDoor
+        : Outside, Interfaces.IHasExteriorDoor
+    {
+        string doorDescription;
+        public OutsideWithDoor(string name, bool hot, string doorDescription) : base(name, hot)
+        {
+            this.doorDescription = doorDescription;
+        }
+
+        public string DoorDescription
+        {
+            get
+            {
+                return doorDescription;
+            }
+        }
+
+        public Location DoorLocation
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ BuildingHouse/BuisnesLogic/HouseItems/RoomWithDoor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuisnesLogic.HouseItems.Base;
+
+namespace BuisnesLogic.HouseItems
+{
+    public class RoomWithDoor
+        : Room, Interfaces.IHasExteriorDoor
+    {
+        string doorDescription;
+
+        public RoomWithDoor(string name, string decoration, string doorDescription) : base(name, decoration)
+        {
+            this.doorDescription = doorDescription;
+        }
+
+        public string DoorDescription
+        {
+            get
+            {
+                return doorDescription;
+            }
+        }
+
+        public Location DoorLocation
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
