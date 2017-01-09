4a2664395c056813534fcec14490bb75caae8d68
nester <nesterd85@gmail.com>
2017-01-09 13:41:12 +0700

    почти описан класс Oppenent

--- BuildingHouse/BuisnesLogic/OpponentLogic/Opponent.cs
+++ BuildingHouse/BuisnesLogic/OpponentLogic/Opponent.cs
@@ -1,4 +1,5 @@
 ﻿using BuisnesLogic.HouseItems.Base;
+using BuisnesLogic.HouseItems.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,48 @@ namespace BuisnesLogic.OpponentLogic
     {
         Location myLocation;
         Random random;
+        bool hide = false;
+        int numberOfTurns;
 
-        public Opponent(Location startPosition)
+        public Opponent(Location startPosition, int numberOfTurns)
         {
             myLocation = startPosition;
             random = new Random();
+            this.numberOfTurns = numberOfTurns;
+        }
+
+        private void Move()
+        {
             
+            if(myLocation is IHasExteriorDoor && random.Next(2) == 1)
+            {
+                    IHasExteriorDoor hasDoor = myLocation as IHasExteriorDoor;
+                    myLocation = hasDoor.DoorLocation;
+                    return;
+            }
+
+            Location[] exits = myLocation.exits;
+            int index = random.Next(exits.Length);
+            myLocation = exits[index];
+        }
+
+        private void NextAction()
+        {
+            if (myLocation is IHidingPlace && random.Next(2) == 1)
+            {
+                hide = true;
+                return;
+            }
+            Move();
+        }
+
+        public Location Hiding()
+        {
+            while(!hide)
+            {
+                NextAction();
+            }
+            return myLocation;
         }
     }
 }
